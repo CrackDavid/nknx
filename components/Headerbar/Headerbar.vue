dd414db671b25d0ae46a42d16146991c7eb636df
Christian Busch <kontakt@christian-busch.com>
2019-02-28 20:43:47 +0100

    feat: search bar in header and some buttons

--- components/Headerbar/Headerbar.vue
+++ components/Headerbar/Headerbar.vue
@@ -1,18 +1,47 @@
 <template>
   <div>
-      <vs-row class="headerbar">
-          <vs-col vs-type="flex" vs-align="center" vs-w="10">
-              <align-left-icon class="headerbar-toggle" @click="toggleSidebar"></align-left-icon>
-          </vs-col>
-      </vs-row>
+    <vs-row class="headerbar">
+      <vs-col vs-type="flex" vs-align="center" vs-justify="left" vs-w="4" class="headerbar-left">
+        <span class="headerbar-item">
+          <align-left-icon class="headerbar-toggle" @click="toggleSidebar"></align-left-icon>
+        </span>
+        <input
+          class="headerbar-search-input"
+          type="text"
+          name="search"
+          placeholder="Search by Block, Hash or Address"
+        >
+      </vs-col>
+      <vs-col
+        vs-type="flex"
+        vs-align="center"
+        vs-justify="flex-end"
+        vs-w="8"
+        class="headerbar-right"
+      >
+        <span class="headerbar-item">
+          <button class="headerbar-button">
+            <bell-icon></bell-icon>
+          </button>
+        </span>
+        <span class="headerbar-item"></span>
+        <span class="headerbar-item">
+          <button class="headerbar-button">
+            <moon-icon></moon-icon>
+          </button>
+        </span>
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 <script>
-import { AlignLeftIcon } from 'vue-feather-icons'
+import { AlignLeftIcon, MoonIcon, BellIcon } from 'vue-feather-icons'
 
 export default {
   components: {
-    AlignLeftIcon
+    AlignLeftIcon,
+    MoonIcon,
+    BellIcon
   },
   methods: {
     toggleSidebar() {
