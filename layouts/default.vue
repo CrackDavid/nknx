b5bbf5bf3d46364b24af6fadce9d9ff6fe3aec22
Andrey <andrtaw1@gmail.com>
2019-02-28 14:33:36 +0300

    fix: preloader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
-    <preloader/>
-    <vs-row>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
-        <Topbar/>
-      </vs-col>
-    </vs-row>
-    <vs-row>
-      <Sidebar/>
-      <div class="main">
-        <vs-row>
-          <vs-col vs-w="12">
-            <headerbar/>
-          </vs-col>
-        </vs-row>
-        <vs-row>
-          <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
-            <nuxt/>
-          </vs-col>
-        </vs-row>
-      </div>
-    </vs-row>
+    <preloader v-if="!price || !networkStatus || !networkNodes"/>
+    <div class="page-wrapper" :class="{'preloading' : !price || !networkStatus || !networkNodes}">
+      <vs-row>
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <Topbar/>
+        </vs-col>
+      </vs-row>
+      <vs-row>
+        <Sidebar/>
+        <div class="main">
+          <vs-row>
+            <vs-col vs-w="12">
+              <headerbar/>
+            </vs-col>
+          </vs-row>
+          <vs-row>
+            <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+              <nuxt/>
+            </vs-col>
+          </vs-row>
+        </div>
+      </vs-row>
+    </div>
   </div>
 </template>
 
@@ -30,12 +32,19 @@ import Topbar from '~/components/Topbar/Topbar.vue'
 import Sidebar from '~/components/Sidebar/Sidebar.vue'
 import Headerbar from '~/components/Headerbar/Headerbar.vue'
 
+import { mapGetters } from 'vuex'
+
 export default {
   components: {
     Preloader,
     Topbar,
     Sidebar,
     Headerbar
-  }
+  },
+  computed: mapGetters({
+    price: 'price/get',
+    networkStatus: 'network/getNetworkStatus',
+    networkNodes: 'network/getNetworkNodes'
+  })
 }
 </script>
