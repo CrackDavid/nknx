671391340870e514d5e78c84157d837fd51f02e5
Andrey <andrtaw1@gmail.com>
2019-03-02 01:54:30 +0300

    feat: add burger animation

--- components/Headerbar/Headerbar.vue
+++ components/Headerbar/Headerbar.vue
@@ -3,7 +3,20 @@
     <vs-row class="headerbar">
       <vs-col vs-type="flex" vs-align="center" vs-justify="left" vs-w="4" class="headerbar-left">
         <span class="headerbar-item">
-          <align-left-icon class="headerbar-toggle" @click="toggleSidebar"></align-left-icon>
+          <div class="headerbar-toggle" @click="toggleSidebar">
+            <svg viewBox="300 20 400 600">
+              <path
+                d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200"
+                id="top"
+              ></path>
+              <path d="M300,320 L540,320" id="middle"></path>
+              <path
+                d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190"
+                id="bottom"
+                transform="translate(480, 320) scale(1, -1) translate(-480, -318) "
+              ></path>
+            </svg>
+          </div>
         </span>
         <input
           class="headerbar-search-input"
@@ -35,17 +48,19 @@
   </div>
 </template>
 <script>
-import { AlignLeftIcon, MoonIcon, BellIcon } from 'vue-feather-icons'
+import { MoonIcon, BellIcon } from 'vue-feather-icons'
 
 export default {
   components: {
-    AlignLeftIcon,
     MoonIcon,
     BellIcon
   },
   methods: {
     toggleSidebar() {
       this.$store.dispatch('sidebar/toggleSidebar')
+      const burger = document.getElementsByClassName('headerbar-toggle')[0]
+      const crossClass = 'cross'
+      burger.classList.toggle(crossClass)
     }
   }
 }
