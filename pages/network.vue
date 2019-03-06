dab4a20f9df8535583e87fd31827e26a3bb95e2f
Andrey <andrtaw1@gmail.com>
2019-03-06 21:44:41 +0300

    feat: add network-status widget, add blinks modifier

--- pages/network.vue
+++ pages/network.vue
@@ -1,9 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <div class="row">
+      <div class="col col_xs_3">
+        <NetworkStatus/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import NetworkStatus from '~/components/Widgets/Network/NetworkStatus/NetworkStatus.vue'
+
 export default {
-  auth: false
+  auth: false,
+  components: {
+    NetworkStatus
+  }
 }
 </script>
