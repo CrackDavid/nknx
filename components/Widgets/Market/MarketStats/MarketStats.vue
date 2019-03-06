7f8de2ab7594e90cba1a08e16575c9c114fe985f
Christian Busch <kontakt@christian-busch.com>
2019-03-06 18:38:41 +0100

    refactored MarketStats again

--- components/Widgets/Market/MarketStats/MarketStats.vue
+++ components/Widgets/Market/MarketStats/MarketStats.vue
@@ -4,37 +4,15 @@
       <div class="market-stats__market-data col col_xs_4">
         <div class="market-data__heading">
           <h3 class="market-data__title">NKN Market Statistic</h3>
-          <div class="market-data__rank">Rank {{price.cmc_rank}}</div>
+          <div class="market-data__rank">Rank {{cmcRank}}</div>
         </div>
         <div class="market-data__market-numbers">
-          <div class="market-numbers__item">
-            <div class="market-numbers__name">{{$t('price')}}</div>
-            <div class="market-numbers__value">${{price.prices[0].price.toFixed(4)}}</div>
-          </div>
-          <div class="market-numbers__item">
-            <div class="market-numbers__name">{{$t('change_24h')}}</div>
-            <div
-              class="market-numbers__value"
-              :class="{'market-numbers__value_positive': price.prices[0].percent_change_24h > 0 | 'market-numbers__value_negative'}"
-            >
-              <span
-                class="fe market-numbers__icon"
-                :class="{'fe-trending-up': price.prices[0].percent_change_24h > 0 | 'fe-trending-down'}"
-              />
-              {{price.prices[0].percent_change_24h.toFixed(2) + '%'}}
+          <div class="market-numbers__item" v-for="item in items" :key="item.title">
+            <div class="market-numbers__name">{{item.title}}</div>
+            <div class="market-numbers__value" :class="item.class">
+              <span class="fe market-numbers__icon" :class="item.icon" v-if="item.icon"/>
+              {{item.data}}
             </div>
-          </div>
-          <div class="market-numbers__item">
-            <div class="market-numbers__name">{{$t('marketcap')}}</div>
-            <div class="market-numbers__value">${{numberWithCommas(price.prices[0].market_cap)}}</div>
-          </div>
-          <div class="market-numbers__item">
-            <div class="market-numbers__name">{{$t('daily_vol')}}</div>
-            <div class="market-numbers__value">${{numberWithCommas(price.prices[0].volume_24h)}}</div>
-          </div>
-          <div class="market-numbers__item">
-            <div class="market-numbers__name">{{$t('nkn_eth')}}</div>
-            <div class="market-numbers__value">${{price.prices[0].price.toFixed(7)}}</div>
           </div>
         </div>
       </div>
@@ -52,20 +30,59 @@ import { mapGetters } from 'vuex'
 
 export default {
   components: {
-    PriceChart,
-    Card
+    Card,
+    PriceChart
   },
   computed: mapGetters({
     price: 'price/get'
   }),
   data: () => {
     return {
-      cmcRank: 0
+      cmcRank: 0,
+      items: {
+        price: {
+          title: 'Price',
+          data: null
+        },
+        change24h: {
+          title: 'Change 24h',
+          data: null
+        },
+        marketcap: {
+          title: 'Marketcap',
+          data: null
+        },
+        dailyVolume: {
+          title: 'Daily Volume',
+          data: null
+        },
+        nknEth: {
+          title: 'NKN/ETH',
+          data: null
+        }
+      }
     }
   },
 
   destroyed() {},
-  mounted: function() {},
+  mounted: function() {
+    this.items.price.data = '$' + this.price.prices[0].price.toFixed(4)
+    if (this.price.prices[0].percent_change_24h > 0) {
+      this.items.change24h.class = 'market-numbers__value_positive'
+      this.items.change24h.icon = 'fe-trending-up'
+    } else {
+      this.items.change24h.class = 'market-numbers__value_negative'
+      this.items.change24h.icon = 'fe-trending-down'
+    }
+    this.cmcRank = this.price.cmc_rank
+    this.items.change24h.data =
+      this.price.prices[0].percent_change_24h.toFixed(2) + '%'
+    this.items.marketcap.data =
+      '$' + this.numberWithCommas(this.price.prices[0].market_cap)
+    this.items.dailyVolume.data =
+      '$' + this.numberWithCommas(this.price.prices[0].volume_24h)
+    this.items.nknEth.data = '$' + this.price.prices[1].price.toFixed(7)
+  },
   methods: {
     numberWithCommas(x) {
       x = x.toFixed(0)
