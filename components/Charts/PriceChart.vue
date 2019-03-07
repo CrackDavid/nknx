c7b73bbcae36a6dd508bc82984ed389c325e6b67
Andrey <andrtaw1@gmail.com>
2019-03-07 03:33:21 +0300

    feat: a few market chart styles

--- components/Charts/PriceChart.vue
+++ components/Charts/PriceChart.vue
@@ -19,18 +19,26 @@ export default {
     const chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart)
     chart.paddingRight = 20
 
-    let usdPrice = this.dailyHistoryPrice.USD
+    let nknUsd = this.dailyHistoryPrice.USD
     let nknEth = this.dailyHistoryPrice.ETH
-    usdPrice = usdPrice.slice(0, 7)
+    nknUsd = nknUsd.slice(0, 7)
     nknEth = nknEth.slice(0, 7)
 
-    const data = nknEth
-    console.log(usdPrice)
+    const nknPrice = []
+    for (let i = 0; i < nknEth.length; i++) {
+      nknPrice.push({
+        date: nknEth[i].date,
+        eth: nknEth[i].price,
+        usd: nknUsd[i].price
+      })
+    }
+
+    const data = nknPrice
     chart.data = data
 
     const dateAxis = chart.xAxes.push(new am4charts.DateAxis())
     dateAxis.fontSize = 10
-    dateAxis.color = '#a2a5b9'
+    dateAxis.renderer.labels.template.fill = am4core.color('#a2a5b9')
     dateAxis.renderer.grid.template.location = 0.5
     dateAxis.startLocation = 0.5
     dateAxis.endLocation = 0.5
@@ -39,15 +47,32 @@ export default {
 
     const valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
     valueAxis.fontSize = 10
-    valueAxis.color = '#a2a5b9'
+    valueAxis.renderer.labels.template.fill = am4core.color('#a2a5b9')
     valueAxis.tooltip.disabled = false
     valueAxis.renderer.minWidth = 35
 
-    const series = chart.series.push(new am4charts.LineSeries())
-    series.dataFields.dateX = 'date'
-    series.dataFields.valueY = 'price'
+    const valueAxis2 = chart.yAxes.push(new am4charts.ValueAxis())
+    valueAxis2.fontSize = 10
+    valueAxis2.renderer.labels.template.fill = am4core.color('#a2a5b9')
+    valueAxis2.tooltip.disabled = false
+    valueAxis2.renderer.minWidth = 35
+    valueAxis2.position = 'right' // Why it doesn't work? xD
 
-    series.tooltipText = '{valueY.price}'
+    const series1 = chart.series.push(new am4charts.LineSeries())
+    series1.dataFields.dateX = 'date'
+    series1.dataFields.valueY = 'usd'
+    series1.tooltipText = '{valueY.usd}'
+    series1.stroke = am4core.color('#5FCFF9') // This thing have to be gradient
+    series1.strokeWidth = 3
+
+    const series2 = chart.series.push(new am4charts.LineSeries())
+    series2.dataFields.dateX = 'date'
+    series2.dataFields.valueY = 'eth'
+    series2.yAxis = valueAxis2
+    series2.tooltipText = '{valueY.eth}'
+    series2.stroke = am4core.color('#DD5E89') // This thing have to be gradient
+    series2.strokeWidth = 3
+
     chart.cursor = new am4charts.XYCursor()
 
     this.chart = chart
