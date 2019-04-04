3eec51d8ff075d8b98a5b00c9eaafa681e8a602f
Andrey <andrtaw1@gmail.com>
2019-04-05 02:00:08 +0300

    feat: add node manager buttons

--- pages/node-manager.vue
+++ pages/node-manager.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="node-manager">
     <div class="node-manager__top">
-      <div class="node-manager__header">
-        <div class="row">
-          <div class="col col_xs_9">
-            <div class="node-manager__left">
-              <div class="node-manager__title">{{$t('myNodes')}}</div>
-              <div class="node-manager__status">
-                <span class="node-manager__indicator node-manager__indicator_positive"></span> All nodes online
-              </div>
-            </div>
+      <div class="node-manager__header col col_xs_12">
+        <div class="node-manager__left">
+          <div class="node-manager__title">{{$t('myNodes')}}</div>
+          <div class="node-manager__status">
+            <span class="node-manager__indicator node-manager__indicator_positive"></span> All nodes online
           </div>
-          <div class="col col_xs_3">
-            <div class="node-manager__right"></div>
-          </div>
+        </div>
+        <div class="node-manager__right">
+          <button
+            class="node-manager__button btn btn_type_default btn_theme_primary btn_size_small btn_icon_chart"
+            disabled
+          ></button>
+          <button
+            class="node-manager__button btn btn_type_default btn_theme_primary btn_size_small btn_icon_report"
+            disabled
+          >{{$t('miningReport')}}</button>
+          <button
+            class="node-manager__button btn btn_type_default btn_theme_primary btn_size_small btn_icon_plus"
+          >{{$t('addNode')}}</button>
         </div>
       </div>
       <NodesAverageStats/>
