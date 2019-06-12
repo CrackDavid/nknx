a8617556be9fc22124420967ab7769f2beb1d6ca
Andrey <andrtaw1@gmail.com>
2019-06-12 12:53:58 +0300

    feat: add initial setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,18 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900&amp;subset=cyrillic'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
@@ -23,7 +34,11 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: [
+    '~/assets/css/reset.css',
+    '~/assets/css/feather.css',
+    '@/assets/scss/main.scss'
+  ],
   /*
    ** Plugins to load before mounting the App
    */
@@ -35,13 +50,43 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources',
+    'nuxt-svg-loader',
+    [
+      '@nuxtjs/moment',
+      {
+        locales: [
+          'de',
+          'ru',
+          'fr',
+          'tr',
+          'es',
+          'nl',
+          'hu',
+          'ja',
+          'fi',
+          'vi',
+          'sk',
+          'zh-cn'
+        ],
+        defaultLocale: 'en'
+      }
+    ]
   ],
+  styleResources: {
+    scss: [
+      '~/assets/scss/_app.variables.scss',
+      '~/assets/scss/_app.mixins.scss'
+    ]
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    baseURL: 'https://api2.nknx.org/'
+  },
   /*
    ** Build configuration
    */
@@ -49,6 +94,16 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
