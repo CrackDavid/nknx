f9bd4b834c3c0695fb1dc6b03f45e1e6169789b3
Andrey <andrtaw1@gmail.com>
2019-07-27 18:14:49 +0300

    fix: table row loader

--- components/Loaders/TableRowLoader/TableRowLoader.vue
+++ components/Loaders/TableRowLoader/TableRowLoader.vue
@@ -1,21 +1,15 @@
 <template>
-  <tr style="padding-top: 1px;">
-    <td v-for="i in count" :key="i">
+  <tr>
+    <td v-for="i in count" :key="i" style="padding: 0 24px;">
       <ContentLoader
-        class="card_col_12"
-        style="margin-top: 8px"
+        style="height: 70px; width: 150px; position: relative; top: 8px;"
         primaryColor="#BDBDBD"
         :speed="1"
-        :height="62"
         :width="1200"
       >
-        <rect x="90" y="31" rx="3" ry="3" width="85" height="6" />
-        <rect x="0" y="80" rx="3" ry="3" width="350" height="6" />
-        <rect x="0" y="100" rx="3" ry="3" width="380" height="6" />
-        <rect x="0" y="120" rx="3" ry="3" width="201" height="6" />
+        <rect x="90" y="31" rx="3" ry="3" width="180" height="6" />
         <circle cx="30.2" cy="32.2" r="30.2" />
-        <rect x="177" y="31" rx="3" ry="3" width="85" height="6" />
-        <rect x="288" y="31" rx="3" ry="3" width="250" height="6" />
+        <rect x="300" y="31" rx="3" ry="3" width="250" height="6" />
         <rect x="568" y="31" rx="3" ry="3" width="250" height="6" />
         <rect x="866" y="31" rx="3" ry="3" width="130" height="6" />
         <rect x="1039" y="31" rx="3" ry="3" width="85" height="6" />
