2062384ba1560566d80125051fa6d96df527d2cb
Andrey <andrtaw1@gmail.com>
2019-08-21 12:11:39 +0300

    fix: wallet tx title

--- components/TransactionTypeTitle/TransactionTypeTitle.vue
+++ components/TransactionTypeTitle/TransactionTypeTitle.vue
@@ -20,49 +20,58 @@ export default {
       className: ''
     }
   },
-  mounted: function() {
-    const type = this.type
-    switch (type) {
-      case 'all':
-        this.title = 'allTransactions'
-        this.className = 'all'
-        break
-      case 'COINBASE_TYPE':
-        this.title = 'miningReward'
-        this.className = 'reward'
-        break
-      case 'TRANSFER_ASSET_TYPE':
-        this.title = 'transfer'
-        this.className = 'transfer'
-        break
-      case 'SIG_CHAIN_TXN_TYPE':
-        this.title = 'signatureChain'
-        this.className = 'sigchain'
-        break
-      case 'SUBSCRIBE_TYPE':
-        this.title = 'subscription'
-        this.className = 'subscription'
-        break
-      case 'REGISTER_NAME_TYPE':
-        this.title = 'walletNameRegistration'
-        this.className = 'wallet-name-registration'
-        break
-      case 'TRANSFER_NAME_TYPE':
-        this.title = 'walletNameTransfer'
-        this.className = 'wallet-name-transfer'
-        break
-      case 'DELETE_NAME_TYPE':
-        this.title = 'walletNameDeletion'
-        this.className = 'wallet-name-deletion'
-        break
-      case 'GENERATE_ID_TYPE':
-        this.title = 'generateId'
-        this.className = 'generate-id'
-        break
-      default:
-        return false
+  watch: {
+    type: function(newVal, oldVal) {
+      this.getTxTitle()
     }
   },
-  methods: {}
+  mounted() {
+    this.getTxTitle()
+  },
+  methods: {
+    getTxTitle() {
+      const type = this.type
+      switch (type) {
+        case 'all':
+          this.title = 'allTransactions'
+          this.className = 'all'
+          break
+        case 'COINBASE_TYPE':
+          this.title = 'miningReward'
+          this.className = 'reward'
+          break
+        case 'TRANSFER_ASSET_TYPE':
+          this.title = 'transfer'
+          this.className = 'transfer'
+          break
+        case 'SIG_CHAIN_TXN_TYPE':
+          this.title = 'signatureChain'
+          this.className = 'sigchain'
+          break
+        case 'SUBSCRIBE_TYPE':
+          this.title = 'subscription'
+          this.className = 'subscription'
+          break
+        case 'REGISTER_NAME_TYPE':
+          this.title = 'walletNameRegistration'
+          this.className = 'wallet-name-registration'
+          break
+        case 'TRANSFER_NAME_TYPE':
+          this.title = 'walletNameTransfer'
+          this.className = 'wallet-name-transfer'
+          break
+        case 'DELETE_NAME_TYPE':
+          this.title = 'walletNameDeletion'
+          this.className = 'wallet-name-deletion'
+          break
+        case 'GENERATE_ID_TYPE':
+          this.title = 'generateId'
+          this.className = 'generate-id'
+          break
+        default:
+          return false
+      }
+    }
+  }
 }
 </script>
