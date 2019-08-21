5ed32ba4bb326e7f675c12a529b0491e3bd1aa48
Andrey <andrtaw1@gmail.com>
2019-08-21 17:50:40 +0300

    feat: add mobile countries

--- components/CountryList/CountryList.vue
+++ components/CountryList/CountryList.vue
@@ -14,12 +14,16 @@
         <span v-else>{{$t('viewTop')}}</span>
       </div>
     </div>
-    <div class="overflow-x">
+    <div class="country-list__stats">
+      <div class="country-list__stats-title">{{$t('totalCountries')}}</div>
+      <div class="country-list__stats-value">{{countries.length}}</div>
+    </div>
+    <div v-if="$mq !== 'xs' && $mq !== 'sm' && $mq !== 'md'" class="overflow-x">
       <table class="table">
         <thead class="table__header">
           <tr class="table__row">
             <th class="table__title" style="width: 30%;">{{$t('country')}}</th>
-            <th class="table__title">{{$t('currentNodeCount')}}</th>
+            <th class="table__title">{{$t('nodeCount')}}</th>
             <th
               class="table__title table__item text_align_right"
               style="width: 20%;"
@@ -28,9 +32,9 @@
         </thead>
         <tbody class="table__body">
           <tr v-for="item in filterCountries" :key="item.country" class="table__row">
-            <td class="table__item">
+            <td class="table__item country-list__country text_wrap_none">
               <CountryFlag class="country-list__flag" :country="item.country_code" size="normal" />
-              <span class="country-list__country text_wrap_none">{{item.country_name}}</span>
+              <span style="margin-left:40px">{{item.country_name}}</span>
             </td>
             <td class="table__item">
               <span class="country-list__percentage" :style="{width: item.percent+'%'}"></span>
@@ -42,6 +46,32 @@
           </tr>
         </tbody>
       </table>
+    </div>
+    <div v-else>
+      <table class="table">
+        <thead class="table__header">
+          <tr class="table__row">
+            <th class="table__title">{{$t('country')}}</th>
+            <th class="table__title text_align_right">{{$t('nodeCount')}}</th>
+          </tr>
+        </thead>
+        <tbody class="table__body">
+          <tr v-for="item in filterCountries" :key="item.country" class="table__row">
+            <td class="table__item country-list__country text_wrap_none">
+              <CountryFlag class="country-list__flag" :country="item.country_code" size="normal" />
+              <span style="margin-left:40px">{{item.country_name}}</span>
+              <div class="country-list__percentage" :style="{width: item.percent+'%'}"></div>
+            </td>
+            <td
+              class="table__item text_align_right"
+            >{{item.count}} {{'('+(Number(item.percent)).toFixed(2)+'%)'}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="card-footer" style="padding-bottom: 16px;" @click="toggleCountries">
+        <span v-if="!fullCountries">{{$t('viewAll')}}</span>
+        <span v-else>{{$t('viewTop')}}</span>
+      </div>
     </div>
   </Card>
 </template>
